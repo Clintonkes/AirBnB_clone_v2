444f7897bf0ab1e2142e20ec6800152441107be8
Clintonkes <clintonemeka05@gmail.com>
2022-10-26 09:29:02 +0000

    Web Static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,7 +2,8 @@
 # Fabfile to generates a .tgz archive from the contents of web_static.
 import os.path
 from datetime import datetime
-from fabric.api import
+from fabric.api import local
+
 
 def do_pack():
     """Create a tar gzipped archive of the directory web_static."""
